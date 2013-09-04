8f613f96e88fb6f9b1a61505b3afcf7fa46398b7
Josh Earl <joshearl928@gmail.com>
2013-09-04 08:18:20 -0400

    Starting over.

--- KeyBindingListener.py
+++
@@ -1,5 +0,0 @@
-import sublime, sublime_plugin
-
-class KeyBindingListener(sublime_plugin.EventListener):
-    def on_window_command(self, window, name, args):
-        print("The command name is: " + name)
