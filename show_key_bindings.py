0f89032034c18290a5055eb0a475c3cd661c41e5
Josh Earl <joshearl928@gmail.com>
2013-09-06 09:47:43 -0400

    Filter down the list of files to relevant keymaps
    From the list of all files contained in packages, print out the names of
    all default and OS-specific keymaps.

--- show_key_bindings.py
+++ show_key_bindings.py
@@ -10,12 +10,19 @@ for mod in reload_mods:
     print "Reloading module %s ..." % mod
     reload(sys.modules[mod])
 
+PLATFORM = sublime.platform().title()
+if PLATFORM == "Osx":
+    PLATFORM = "OSX"
+
 class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
     def run(self):
         # window.run_command("show_key_bindings")
         packages = get_packages_list()
-        print packages
         for package in packages:
             file_list = list_package_files(package)
-            print file_list
+            platform_keymap = "default (%s).sublime-keymap" % (PLATFORM.lower())
+            for filename in file_list:
+                if filename.lower().endswith("default.sublime-keymap")or \
+                filename.lower().endswith(platform_keymap):
+                    print filename
         
