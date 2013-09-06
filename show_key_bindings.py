3bb7663af8b1a12ce0c3d814ef2426c9ba49fc14
Josh Earl <joshearl928@gmail.com>
2013-09-06 10:28:45 -0400

    Refactor!
    Indentation was starting to get too deep, so I did some refactoring and
    renaming.

--- show_key_bindings.py
+++ show_key_bindings.py
@@ -17,12 +17,26 @@ if PLATFORM == "Osx":
 class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
     def run(self):
         # window.run_command("show_key_bindings")
-        packages = get_packages_list()
-        for package in packages:
-            file_list = list_package_files(package)
-            platform_keymap = "default (%s).sublime-keymap" % (PLATFORM.lower())
-            for filename in file_list:
-                if filename.lower().endswith("default.sublime-keymap")or \
-                filename.lower().endswith(platform_keymap):
-                    print filename
+        keymap_list = self.get_keymap_files()
+        print keymap_list
+
+    def get_keymap_files(self):
+        keymap_list = []
         
+        package_list = get_packages_list()
+        for package in package_list:
+            package_keymap_list = self.get_keymaps_from(package)
+            if (package_keymap_list):
+                keymap_list.append(package_keymap_list)
+        
+        return keymap_list
+            
+    def get_keymaps_from(self, package):
+        file_list = list_package_files(package)
+        platform_keymap_pattern = "default (%s).sublime-keymap" % (PLATFORM.lower())
+        package_keymap_list = []
+        for filename in file_list:
+            if filename.lower().endswith("default.sublime-keymap")or \
+            filename.lower().endswith(platform_keymap_pattern):
+                package_keymap_list.append(filename)
+        return package_keymap_list
