5bcb0c4ba60d0900aa107d962f726a7dab2c3e2e
Josh Earl <joshearl928@gmail.com>
2013-09-06 08:37:25 -0400

    Extract a list of files from each package.

--- lib/package_resources.py
+++ lib/package_resources.py
@@ -1,8 +1,10 @@
 import sublime
 import os
+import re
 
 __all__ = [
-    "get_packages_list"
+    "get_packages_list",
+    "list_package_files"
 ]
 
 def get_packages_list(ignore_packages=True):
@@ -31,3 +33,45 @@ def _get_packages_from_directory(directory, file_ext=""):
 
         package_list.append(package)
     return package_list
+
+def list_package_files(package, ignore_patterns=[]):
+    """
+    List files in the specified package.
+    """
+    package_path = os.path.join(sublime.packages_path(), package, "")
+    path = None
+    file_set = set()
+    file_list = []
+    if os.path.exists(package_path):
+        for root, directories, filenames in os.walk(package_path):
+            temp = root.replace(package_path, "")
+            for filename in filenames:
+                file_list.append(os.path.join(temp, filename))
+
+    file_set.update(file_list)
+
+    file_list = []
+
+    for filename in file_set:
+        if not _ignore_file(filename, ignore_patterns):
+            file_list.append(_normalize_to_sublime_path(filename))
+
+    return sorted(file_list)
+
+def _ignore_file(filename, ignore_patterns=[]):
+    ignore = False
+    directory, base = os.path.split(filename)
+    for pattern in ignore_patterns:
+        if re.match(pattern, base):
+            return True
+
+    if len(directory) > 0:
+        ignore = _ignore_file(directory, ignore_patterns)
+
+    return ignore
+
+def _normalize_to_sublime_path(path):
+    path = os.path.normpath(path)
+    path = re.sub(r"^([a-zA-Z]):", "/\\1", path)
+    path = re.sub(r"\\", "/", path)
+    return path
--- show_key_bindings.py
+++ show_key_bindings.py
@@ -7,5 +7,8 @@ class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
     def run(self):
         # window.run_command("show_key_bindings")
         packages = get_packages_list()
-
         print packages
+        for package in packages:
+            file_list = list_package_files(package)
+            print file_list
+        
