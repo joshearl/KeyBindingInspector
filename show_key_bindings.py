e495499f685e5a324990c4c2dc3db88b1f42a585
Josh Earl <joshearl928@gmail.com>
2013-09-13 08:28:06 -0400

    Add a debug setting.
    This commit shows how to add a custom settings file to your plugin, then
    extract the settings and use them to alter the plugin's behavior.
    
    Here I'm adding a debug setting that defaults to false. Debugging
    messages are printed to the console when the setting is flipped to true.

--- show_key_bindings.py
+++ show_key_bindings.py
@@ -24,19 +24,23 @@ class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
         sublime_plugin.WindowCommand.__init__(self, window)
         self.view = window.active_view()
         self.keyboard_shortcuts_and_commands = []
+        self.plugin_settings = sublime.load_settings("KeyBindingInspector.sublime-settings")
 
     def run(self):
+        self.debug = self.plugin_settings.get("debug", False)
+
         ignored_packages_list = sublime.load_settings(
             "Preferences.sublime-settings").get("ignored_packages", [])
+
         packages_path = sublime.packages_path()
 
-        key_binding_extractor = KeyBindingExtractor(packages_path, ignored_packages_list)
+        key_binding_extractor = KeyBindingExtractor(self.plugin_settings, packages_path, ignored_packages_list)
         key_binding_extractor.start()
         self.handle_key_binding_extraction(key_binding_extractor)
 
     def handle_key_binding_extraction(self, thread, i=0, direction=1):
         if thread.is_alive():
-            print "Thread is running ..."
+            self.debug_log("Thread is running ...")
 
             if (self.view):
                 before = i % 8
@@ -53,7 +57,7 @@ class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
             sublime.set_timeout(lambda: self.handle_key_binding_extraction(thread, i, direction), 100)
             return
 
-        print "Thread finished."
+        self.debug_log("Thread finished.")
 
         if (self.view):
             self.view.erase_status('key_binding_extractor')
@@ -79,20 +83,26 @@ class ShowKeyBindingsCommand(sublime_plugin.WindowCommand):
 
     def run_selected_command(self, selected_command_index):
         if (selected_command_index == -1):
-            print "No command selected."
+            self.debug_log("No command selected.")
             return
-        print "Selected command: " + \
-            str(self.keyboard_shortcuts_and_commands[selected_command_index])
+        self.debug_log("Selected command: " + \
+            str(self.keyboard_shortcuts_and_commands[selected_command_index]))
+
+    def debug_log(self, message):
+        if (self.debug):
+            print message
 
 class KeyBindingExtractor(threading.Thread):
-    def __init__(self, packages_path, ignored_packages_list):
+    def __init__(self, settings, packages_path, ignored_packages_list):
+        self.debug = settings.get("debug", False)
         self.packages_path = packages_path
         self.ignored_packages_list = ignored_packages_list
         self.result = None
         threading.Thread.__init__(self)
 
     def run(self):
-        print "Starting work on background thread ..."
+        if (self.debug):
+            print "Starting work on background thread ..."
         self.result = self.get_key_bindings_list()
 
     def get_key_bindings_list(self):
